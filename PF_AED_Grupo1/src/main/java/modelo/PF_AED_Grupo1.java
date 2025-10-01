cf004837fb775a79b3facf1a805ffc7b0f1c5229
JesusDT <giussepedelgado0704@gmail.com>
2025-10-01 11:35:55 -0500

    Creación demo

---
+++ PF_AED_Grupo1/src/main/java/modelo/PF_AED_Grupo1.java
@@ -0,0 +1,13 @@
+
+package modelo;
+
+/**
+ *
+ * @author Giuss
+ */
+public class PF_AED_Grupo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
