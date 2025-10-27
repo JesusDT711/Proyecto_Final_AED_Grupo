7451365a4f4107fbba3ecf4c490d199e07983d8b
Aaron Diego Palomino Valer <aaron.palomino@yakindustrys.com>
2025-10-26 19:06:48 -0500

    AED-Implementacion de la clase cArreglo

---
+++ PF_AED_Grupo1/src/main/java/controlador/cArreglo.java
@@ -0,0 +1,106 @@
+
+package controlador;
+
+import modelo.cProducto;
+
+public class cArreglo {
+    private cProducto[] productos;
+    private int n;
+    private int capacidad;
+    private double totalVentas; 
+
+    public cArreglo(int capacidad) {
+        this.capacidad = capacidad;
+        this.productos = new cProducto[this.capacidad];
+        this.n = 0;
+        this.totalVentas = 0.0;
+    }
+    
+    public boolean agregar(cProducto p) {
+        boolean exito = false;
+        if (this.n < this.capacidad) {
+            this.productos[this.n] = p;
+            this.n++;
+            exito = true;
+        }
+        return exito;
+    }
+    
+    public cProducto obtener(int indice) {
+        cProducto producto = null;
+        
+        if (indice >= 0 && indice < this.n) {
+            producto = this.productos[indice];
+        }
+        
+        return producto;
+    }
+    
+    public int tamaño() {
+        return this.n;
+    }
+
+    public cProducto buscar(String codigo) {
+        cProducto productoEncontrado = null;
+        int i = 0;
+        boolean encontrado = false;
+        
+        while (i < this.n && !encontrado) {
+            if (this.productos[i].getCodigo().equalsIgnoreCase(codigo)) {
+                productoEncontrado = this.productos[i];
+                encontrado = true;
+            }
+            i++; 
+        }
+        
+        return productoEncontrado;
+    }
+    
+    public boolean eliminar(String codigo) {
+        boolean exito = false;
+        int indiceEncontrado = -1;
+        int i = 0;
+        boolean encontrado = false;
+
+        while (i < this.n && !encontrado) {
+            if (this.productos[i].getCodigo().equalsIgnoreCase(codigo)) {
+                indiceEncontrado = i;
+                exito = true;
+                encontrado = true;
+            }
+            i++;
+        }
+        if (exito) {
+            for (int j = indiceEncontrado; j < this.n - 1; j++) {
+                this.productos[j] = this.productos[j + 1];
+            }
+            
+            this.productos[this.n - 1] = null;
+            this.n--;
+        }
+
+        return exito;
+    }
+
+    public boolean registrarVenta(String codigo, int cantidadComprada) {
+        boolean exito = false;
+        cProducto producto = buscar(codigo);
+
+        if (producto != null) {
+            if (producto.getStock() >= cantidadComprada) {
+                int nuevoStock = producto.getStock() - cantidadComprada;
+                producto.setStock(nuevoStock);
+                
+                double montoDeVenta = producto.getPrecio() * cantidadComprada;
+                this.totalVentas += montoDeVenta;
+                
+                exito = true;
+            }
+        }
+        return exito;
+    }
+    
+    public double getTotalVentas() {
+        return this.totalVentas;
+    }
+}
