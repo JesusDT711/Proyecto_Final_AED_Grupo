dcb84b47884bf8a00380f9421dd56205d62a54ef
Xnhya <lilzeji@gmail.com>
2025-10-30 21:24:14 -0500

    Creación del Formulario Login- Reichel

--- PF_AED_Grupo1/src/main/java/vista/frMClienteAdd.java
+++ PF_AED_Grupo1/src/main/java/vista/frMClienteAdd.java
@@ -8,12 +8,12 @@ package vista;
  *
  * @author adria
  */
-public class fmrClienteAdd extends javax.swing.JFrame {
+public class frmClienteAdd extends javax.swing.JFrame {
 
     /**
      * Creates new form fmrClienteAdd
      */
-    public fmrClienteAdd() {
+    public frmClienteAdd() {
         initComponents();
     }
 
@@ -206,20 +206,23 @@ public class fmrClienteAdd extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(fmrClienteAdd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmClienteAdd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(fmrClienteAdd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmClienteAdd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(fmrClienteAdd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmClienteAdd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(fmrClienteAdd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(frmClienteAdd.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new fmrClienteAdd().setVisible(true);
+                new frmClienteAdd().setVisible(true);
             }
         });
     }
