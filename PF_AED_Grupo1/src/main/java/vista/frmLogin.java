62239a454e5edf11d51c4ee07ed0764c1d379027
Xnhya <lilzeji@gmail.com>
2025-11-02 00:33:28 -0500

    Reichel-Funcionalidad frmLogin

--- PF_AED_Grupo1/src/main/java/vista/frmLogin.java
+++ PF_AED_Grupo1/src/main/java/vista/frmLogin.java
@@ -3,16 +3,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package vista;
-
+import javax.swing.JOptionPane;
+import modelo.cUsuario;
 /**
  *
  * @author adria
  */
 public class frmLogin extends javax.swing.JFrame {
 
-    /**
-     * Creates new form fmrLogin
-     */
+    cUsuario oUsuario = new cUsuario("admin","lavidaescorta123");
+    
     public frmLogin() {
         initComponents();
     }
@@ -31,7 +31,6 @@ public class frmLogin extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         txtUsuario = new javax.swing.JTextField();
         txtContraseña = new javax.swing.JTextField();
-        btnSalir = new javax.swing.JButton();
         btnIngresar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -43,9 +42,12 @@ public class frmLogin extends javax.swing.JFrame {
 
         jLabel3.setText("CONTRASEÑA:");
 
-        btnSalir.setText("Salir");
-
         btnIngresar.setText("Ingresar");
+        btnIngresar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnIngresarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -57,16 +59,14 @@ public class frmLogin extends javax.swing.JFrame {
                     .addComponent(jLabel2)
                     .addComponent(txtUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                        .addGroup(layout.createSequentialGroup()
-                            .addComponent(btnSalir)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(btnIngresar))
-                        .addComponent(txtContraseña, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txtContraseña, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(21, 21, 21)
-                        .addComponent(jLabel1)))
-                .addContainerGap(112, Short.MAX_VALUE))
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(60, 60, 60)
+                        .addComponent(btnIngresar)))
+                .addContainerGap(104, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -81,15 +81,30 @@ public class frmLogin extends javax.swing.JFrame {
                 .addComponent(jLabel3)
                 .addGap(18, 18, 18)
                 .addComponent(txtContraseña, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnSalir)
-                    .addComponent(btnIngresar))
-                .addContainerGap(85, Short.MAX_VALUE))
+                .addGap(32, 32, 32)
+                .addComponent(btnIngresar)
+                .addContainerGap(80, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnIngresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnIngresarActionPerformed
+        if(!txtUsuario.getText().isEmpty() && !txtContraseña.getText().isEmpty()){
+            String usuario = txtUsuario.getText();
+            String contraseña= txtContraseña.getText();
+            if(usuario.equals(oUsuario.getUsuario())&& contraseña.equals(oUsuario.getContraseña())){
+                frmGeneral frmGeneral = new frmGeneral();
+                frmGeneral.setVisible(true);
+                this.setVisible(false);
+              
+            }else{
+                JOptionPane.showMessageDialog(null, "Usuario o Contraseña incorrectos :p");
+            }   
+        }else{
+                JOptionPane.showMessageDialog(null, "Complete todos los campos");
+                    }
+    }//GEN-LAST:event_btnIngresarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -129,7 +144,6 @@ public class frmLogin extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnIngresar;
-    private javax.swing.JButton btnSalir;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
