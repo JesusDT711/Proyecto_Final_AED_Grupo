177f069c1aad436f48c811f15730bcdf2c1cbb45
JesusDT <giussepedelgado0704@gmail.com>
2025-11-02 13:21:03 -0500

    Ajustes leves a frmClienteAdd

--- PF_AED_Grupo1/src/main/java/vista/frmClienteAdd.java
+++ PF_AED_Grupo1/src/main/java/vista/frmClienteAdd.java
@@ -218,6 +218,18 @@ public class frmClienteAdd extends javax.swing.JFrame {
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
